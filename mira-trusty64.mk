07f03ab1379d00ba283e33a3bd7fa6b3dbe5226b
Chris Burbridge <cburbridge@gmail.com>
2014-12-08 15:15:35 +0000

    Add prism and MIRA-trusty package scripts.

---
+++ mira-trusty64.mk
@@ -0,0 +1,34 @@
+##############################################################################
+### CONFIGURATION ###
+# package name
+PKG_NAME:=mira-scitos
+
+# version	
+VERSION:=0.2
+
+# Debian increment (to be bumped when the same version is packaged again)	
+DEBINC:=1
+
+# architecture to be build
+ARCH:=amd64
+
+# license (apache|artistic|bsd|gpl|gpl2|gpl3|lgpl|lgpl2|lgpl3|x11)
+LICENSE:=gpl3
+
+# maintainer email address
+MAINTAINER:=marc@hanheide.net
+
+# all deb packages needed to build this
+APT_INSTALL:=libboost-all-dev \
+	libncurses5-dev libogre-1.8-dev libqwt5-qt4-dev libsqlite3-dev libssl-dev libxml2-dev \
+	libxrandr-dev pyqt4-dev-tools qt4-dev-tools ros-indigo-opencv3  subversion libsvn-dev
+
+## Install prefix WITHOUT leading '/', e.g. 'opt' for '/opt'
+INSTALL_PREFIX:=opt
+
+## This is the install command that will install the package unter the INSTALL_PREFIX 
+INSTALL_COMMAND=bash ./mira-installer-binary.sh ubuntu-1404lts-x64 $(GLOBAL_ROOT)
+
+##############################################################################
+
+include dh_bin_wizard.mk
---
+++ prism-robots-precise.mk
@@ -0,0 +1,32 @@
+##############################################################################
+### CONFIGURATION ###
+# package name
+PKG_NAME:=prism-robots
+
+# version	
+VERSION:=4.2.1
+
+# Debian increment (to be bumped when the same version is packaged again)	
+DEBINC:=1
+
+# architecture to be build
+ARCH:=amd64
+
+# license (apache|artistic|bsd|gpl|gpl2|gpl3|lgpl|lgpl2|lgpl3|x11)
+LICENSE:=gpl3
+
+# maintainer email address
+MAINTAINER:=b.lacerda@cs.bham.ac.uk
+
+# all deb packages needed to build this
+APT_INSTALL:=openjdk-6-jre openjdk-6-jdk
+
+## Install prefix WITHOUT leading '/', e.g. 'opt' for '/opt'
+INSTALL_PREFIX:=opt
+
+## This is the install command that will install the package unter the INSTALL_PREFIX 
+INSTALL_COMMAND=
+
+##############################################################################
+
+include dh_bin_wizard.mk
